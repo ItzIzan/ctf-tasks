67794fcaa9481d65b2ac4dc6e5801eccb1cfa7a3
ItzIzan <91438802+ItzIzan@users.noreply.github.com>
2023-12-04 21:26:41 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -2,12 +2,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/core.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/md5.js"></script>
 
-Welcome to the worst search engine in the world!<br>
-<input type="text" name="search" id="searchid">
-<label for="searchid"> Enter your search query</label><br>
-<input type="submit" value="Submit" onclick = 'searchQuery();' >
-<p id='resid'></p>
-
 
 
 <br><br><br><br><br><br><br><br><br>
@@ -16,10 +10,11 @@ The admin zone contains the flags, Please input the password:<br>
 <input type="password" name="password" id="passwordid">
 <label for="password"> Type the password here</label><br>
 <input type="submit" value="Submit" onclick = 'checkPassword();' >
+<a href="index.php">Reset</a>
 
 <script>
 function checkPassword (){
-if (CryptoJS.MD5(document.getElementById('passwordid').value) == 'be2f09f22ccdabad2d27dcd5d50c20a4') {
+if (CryptoJS.MD5(document.getElementById('passwordid').value) == '092fe31229350237d10d885565111db2') {
     location.href="secure.php";
   }
 } 
